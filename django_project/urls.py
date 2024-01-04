d4b5513ab477048c2c24e718de5693432f95df12
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-01-04 16:32:39 +0600

    going to implement djosher

--- django_project/urls.py
+++ django_project/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
+    path(r"^auth/", include("djoser.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
